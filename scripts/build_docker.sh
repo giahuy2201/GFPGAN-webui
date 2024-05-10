4b536b8639a3e55306f1b5c081d233e07978dec7
Huy Nguyen <giahuy2201@icloud.com>
2024-05-10 13:19:33 -0400

    Fix docker build script

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -1,16 +1,19 @@
+poetry export --without-hashes --format requirements.txt > requirements.txt
+
 echo "Building cpu-only Docker image"
 docker build \
-    --platform=linux/${TARGETARCH} \
+    --platform=linux/amd64 \
+    --target runtime-cpu \
     -f Dockerfile \
-    -t giahuy2201:${TARGETARCH} \
+    -t giahuy2201:gfpgan-webui \
     .
 echo "Done"
 
 echo "Building rocm Docker image"
 docker build \
-    --platform=linux/${TARGETARCH} \
+    --platform=linux/amd64 \
     --target runtime-rocm \
     -f Dockerfile \
-    -t giahuy2201:${TARGETARCH} \
+    -t giahuy2201:gfpgan-rocm-webui \
     .
 echo "Done"
