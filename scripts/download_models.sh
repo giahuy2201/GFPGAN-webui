6a24a2ff815892b8972b85f6eeabdd7df66f5a38
Huy Nguyen <giahuy2201@icloud.com>
2025-01-17 03:11:00 -0500

    Add script to download all models

---
+++ scripts/download_models.sh
@@ -0,0 +1,9 @@
+
+wget 'https://public-vigen-video.oss-cn-shanghai.aliyuncs.com/robin/models/RetinaFace-R50.pth' -O weights/RetinaFace-R50.pth
+wget 'https://public-vigen-video.oss-cn-shanghai.aliyuncs.com/robin/models/ParseNet-latest.pth' -O weights/ParseNet-latest.pth
+wget 'https://public-vigen-video.oss-cn-shanghai.aliyuncs.com/robin/models/realesrnet_x4.pth' -O weights/realesrnet_x4.pth
+wget 'https://public-vigen-video.oss-cn-shanghai.aliyuncs.com/robin/models/GPEN-BFR-512.pth' -O weights/GPEN-BFR-512.pth
+wget 'https://public-vigen-video.oss-cn-shanghai.aliyuncs.com/robin/models/GPEN-Colorization-1024.pth' -O weights/GPEN-Colorization-1024.pth
+
+wget 'https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.1/RealESRGAN_x2plus.pth' -O weights/RealESRGAN_x2plus.pth
+wget 'https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.4.pth' -O weights/GFPGANv1.4.pth
