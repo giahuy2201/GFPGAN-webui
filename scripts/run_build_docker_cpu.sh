c9aa49526bfb669aa57b86d699f0ffc52694cf6b
Huy Nguyen <giahuy2201@icloud.com>
2025-01-17 02:52:59 -0500

    Add script to build and run docker in one comand

---
+++ scripts/run_build_docker_cpu.sh
@@ -0,0 +1,13 @@
+poetry export --without-hashes --format requirements.txt > requirements.txt
+
+echo "Building cpu-only Docker image"
+docker build \
+    --platform=linux/amd64 \
+    --target runtime-cpu \
+    -f Dockerfile \
+    -t giahuy2201/gfpgan-webui \
+    .
+echo "Done"
+
+docker compose up -d
+docker logs gfpgan-test -f
