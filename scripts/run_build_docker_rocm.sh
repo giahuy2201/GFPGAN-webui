5308636b0bddd88c2137bfd45798ffafec46beb4
Huy Nguyen <giahuy2201@icloud.com>
2025-01-19 13:49:03 -0500

    Add single script build and run for ROCm docker

---
+++ scripts/run_build_docker_rocm.sh
@@ -0,0 +1,13 @@
+poetry export --without-hashes --format requirements.txt > requirements.txt
+
+echo "Building rocm Docker image"
+docker build \
+    --platform=linux/amd64 \
+    --target runtime-rocm \
+    -f Dockerfile \
+    -t giahuy2201/gfpgan-webui:rocm \
+    .
+echo "Done"
+
+docker compose up -d
+docker logs gfpgan-rocm -f
