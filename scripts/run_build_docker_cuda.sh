7bba2b02120497aa3d434e422159baaebeb1a62a
Huy Nguyen <giahuy2201@icloud.com>
2025-02-09 00:12:42 -0500

    Add cuda docker dev script

---
+++ scripts/run_build_docker_cuda.sh
@@ -0,0 +1,13 @@
+poetry export --without-hashes --format requirements.txt > requirements.txt
+
+echo "Building cuda Docker image"
+docker build \
+    --platform=linux/amd64 \
+    --target runtime-cuda \
+    -f Dockerfile \
+    -t giahuy2201/gfpgan-webui:cuda \
+    .
+echo "Done"
+
+docker compose up -d
+docker logs gfpgan-test -f
